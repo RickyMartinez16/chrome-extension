a995cca80caa821721092498453331a23e992a68
Ricky Martinez <RMartinezW16@gmail.com>
2022-07-11 17:29:16 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const saveButtonEl = document.getElementById("input-btn");
+
+saveButtonEl.addEventListener("click", function(){
+    console.log("save button clicked addevent listen")
+})
+
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field
+
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
