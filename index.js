8308f7c3c41ea462353f8e703d958f55ac55c026
Ricky Martinez <RMartinezW16@gmail.com>
2022-07-12 15:02:03 -0400

    local storage

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 
-const saveButtonEl = document.getElementById("input-btn");
-
-saveButtonEl.addEventListener("click", function(){
-    console.log("save button clicked addevent listen")
-})
-
-// Create two variables:
-// myLeads -> should be assigned to an empty array
-// inputEl -> should be assigned to the text input field
-
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
+const saveButtonEl = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+saveButtonEl.addEventListener("click", function(){
+    myLeads.push(inputEl.value);
+    inputEl.value="";
+    renderLeads();
+})
+
+function renderLeads(){
+    let listItems = "";
+
+    for (let i = 0; i < myLeads.length; i++) {
+        // listItems += `<li><a target=_blank rel=noopener noreferrer href='#'>${myLeads[i]}</a></li>`;
+        listItems += `
+        <li>
+            <a target=_blank rel=noopener noreferrer href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>
+    `
+    };
+
+    ulEl.innerHTML = listItems;
+}
